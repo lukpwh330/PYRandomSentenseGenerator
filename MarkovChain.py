221ff0fa4632eabf0cef581e1d22e5d99acb2cba
Jonathan Li <jonathanliworld@gmail.com>
2019-04-04 21:26:03 -0400

    Update MarkovChain.py

--- MarkovChain.py
+++ MarkovChain.py
@@ -1,65 +1,70 @@
-import random, string
+import random
+import string
 
-# function for initializing the random sentence generator
+
+# Function for initializing the random sentence generator
 def initializeWordList():
 
-	# passage text for the markov chain
+	# Passage text for the markov chain
 	with open('data.txt', 'r') as f:
 		passage = f.read().replace("\n", " ").lower()
 	
-	# generate word list from the passage
-	punctuations = string.punctuation.translate(str.maketrans('', '', ",./'")) + "/" # define unwanted punctuations
-	passage = passage.translate(str.maketrans(punctuations, ' ' * len(punctuations))) # remove punctuations from passage text
-	words = passage.replace(",", ", ").replace(".", ". ").split(" ") # get list of word from the passage
+	# Generate word list from the passage
+	punctuations = string.punctuation.translate(str.maketrans('', '', ",./'")) + "/" # Define unwanted punctuations
+	passage = passage.translate(str.maketrans(punctuations, ' ' * len(punctuations))) # Remove punctuations from passage text
+	words = passage.replace(",", ", ").replace(".", ". ").split(" ") # Get list of word from the passage
 	
-	# clean up the word list
+	# Clean up the word list
 	words = [word.strip("'") for word in words]
 	words = ["I" if word == 'i' else word for word in words if len(word) > 0]
 	
-	wordDict = {} # the dictionary
+	wordDict = {} # The dictionary storing words
 	
-	#generate markov chain dictionary
+	# Generate markov chain dictionary
 	for i in range(len(words) - 2):
 	
-		word1, word2 = words[i], words[i + 1] # first and second word of the key
-		key = word1 + " " + word2 # the key of the dictionary
-		wordNext = words[i + 2] # a possible value of the key
+		word1, word2 = words[i], words[i + 1] # First and second word of the key
+		key = word1 + " " + word2 # The key of the dictionary
+		wordNext = words[i + 2] # A possible value of the key
 		
 		if key in wordDict.keys():
-			wordDict[key].append(wordNext) # add value to key if the key exists in the dictionary
+			wordDict[key].append(wordNext) # Add value to key if the key exists in the dictionary
+		
 		else:
-			wordDict[key] = [wordNext] # create a new key if the key is not in the dictionary
+			wordDict[key] = [wordNext] # Create a new key if the key is not in the dictionary
 			
 	return wordDict
 
-# function for generating a random sentence with random length
-def randomSentence(wordDict, minWords, maxWords): # minimum/maximum number of words of the sentence
 
-	# initialize and define variables
-	numWords = random.randrange(minWords, maxWords + 1) # set the number of words of the sentence	
-	word1, word2 = "", "" # initializing the first and second word for the markov chain	
-	result = "" # the resulting sentence
+# Function for generating a random sentence with random length
+def randomSentence(wordDict, minWords, maxWords): # Minimum/maximum number of words of the sentence
+
+	# Initialize and define variables
+	numWords = random.randrange(minWords, maxWords + 1) # Set the number of words of the sentence	
+	word1, word2 = "", "" # Initializing the first and second word for the markov chain	
+	result = "" # The resulting sentence
 	
-	# walk through the markov chain
+	# Walk through the markov chain
 	for i in range(numWords):
 	
-		key = word1 + " " + word2 # key of the dictionary
+		key = word1 + " " + word2 # Key of the dictionary
 		
-		# get random words from the dictionary when the chain ends
+		# Get random words from the dictionary when the chain ends
 		if key not in wordDict:
-			while not word1.endswith('.') and i == 0: # first word is after a period
-				key = random.choice(list(wordDict.keys())) # get random key			
-				word1, word2, = key.split(" ") # get first and second word from the key
+			while not word1.endswith('.') and i == 0: # First word is after a period
+				key = random.choice(list(wordDict.keys())) # Get random key			
+				word1, word2, = key.split(" ") # Get first and second word from the key
 		
 		if i > 0:
-			result += " " # add space after each word in result string
+			result += " " # Add space after each word in result string
 		
-		result += word2.capitalize() if word1.endswith('.') or i == 0 else word2  # add word to string and capitalize the word after a period
-		
-		word1, word2 = word2, random.choice(wordDict[key]) # move to the next state of the markov chain
+		result += word2.capitalize() if word1.endswith('.') or i == 0 else word2  # Add word to string and capitalize the word after a period
+		word1, word2 = word2, random.choice(wordDict[key]) # Move to the next state of the markov chain
 	
 	return result
 
-chain = initializeWordList(); # dictionary for storing markov chain
 
-print(randomSentence(chain, 900, 1000)) # generate a random sentences with length 900 to 1000
+chain = initializeWordList(); # Dictionary for storing markov chain
+
+print(randomSentence(chain, 900, 1000)) # Generate a random sentences with length 900 to 1000
+
